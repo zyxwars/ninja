bce53c79de7629c0312064bfeef44418bccfd7be
zyxwars <zyxwars221@gmail.com>
2021-11-25 22:13:40 +0100

    Make animation loader util

--- utils/get_path.py
+++ utils/get_path.py
@@ -2,6 +2,7 @@ import os
 
 
 def get_path(script_path, relative_path):
+    relative_path = os.path.normpath(relative_path)
     file_dir = os.path.dirname(os.path.realpath(script_path))
 
     return os.path.join(file_dir, relative_path)
